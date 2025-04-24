43749d9eeab96c69265dd2a1ee5cdf83f9719eba
TalTuchman <taltuchman1@gmail.com>
2025-04-24 14:10:08 +0000

    add tickers

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ import streamlit as st
 
 st.title("S&P 500 Stock Filter App")
 
+import pandas as pd
+
+@st.cache_data
+def load_sp500_tickers():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    tables = pd.read_html(url)
+    df = tables[0]  # the first table on the page
+    return df
+
+df_sp500 = load_sp500_tickers()
+tickers = df_sp500["Symbol"].tolist()
+st.subheader("S&P 500 Tickers (First 10)")
+st.write(tickers[:10])
